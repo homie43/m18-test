5a8b2d9ef103ee0edf319ce62dba0b4416785e8f
Vladimir <homie43@yandex.ru>
2022-11-11 13:56:01 +0300

    вынес скрипты в отдельные функции

--- src/js/script.js
+++ src/js/script.js
@@ -7,39 +7,44 @@ const wholeSlider = document.querySelector(".slider__wrapper"),
     btnSubmit = document.querySelector(".connection__button"),
     submitWindow = document.querySelector(".submit");
 
-prev.addEventListener("click", () => {
-    for (let i = 0; i < slides.length; i++) {
-        if (slides[i].classList.contains("slider__item-active") && i !== 0) {
-            slides[i].classList.remove("slider__item-active");
-            slides[i].previousElementSibling.classList.add(
-                "slider__item-active"
-            );
+const mySlider = () => {
+    prev.addEventListener("click", () => {
+        for (let i = 0; i < slides.length; i++) {
+            if (slides[i].classList.contains("slider__item-active") && i !== 0) {
+                slides[i].classList.remove("slider__item-active");
+                slides[i].previousElementSibling.classList.add("slider__item-active");
 
-            if (i === 1) {
-                wholeSlider.style.transform = "translate(0px)";
-            } else {
-                wholeSlider.style.transform = "translate(-25%)";
+                if (i === 1) {
+                    wholeSlider.style.transform = "translate(0px)";
+                } else {
+                    wholeSlider.style.transform = "translate(-25%)";
+                }
             }
         }
-    }
-});
+    });
 
-next.addEventListener("click", () => {
-    for (let k = slides.length - 1; k >= 0; k--) {
-        if (slides[k].classList.contains("slider__item-active") && k !== 2) {
-            slides[k].classList.remove("slider__item-active");
-            slides[k].nextElementSibling.classList.add("slider__item-active");
+    next.addEventListener("click", () => {
+        for (let k = slides.length - 1; k >= 0; k--) {
+            if (slides[k].classList.contains("slider__item-active") && k !== 2) {
+                slides[k].classList.remove("slider__item-active");
+                slides[k].nextElementSibling.classList.add("slider__item-active");
 
-            if (k === 1) {
-                wholeSlider.style.transform = "translate(-50%)";
-            } else {
-                wholeSlider.style.transform = "translate(-25%)";
+                if (k === 1) {
+                    wholeSlider.style.transform = "translate(-50%)";
+                } else {
+                    wholeSlider.style.transform = "translate(-25%)";
+                }
             }
         }
-    }
-});
+    });
+};
 
-btnSubmit.addEventListener("click", (event) => {
-    event.preventDefault();
-    submitWindow.style.display = "block";
-});
+const onSubmit = () => {
+    btnSubmit.addEventListener("click", (event) => {
+        event.preventDefault();
+        submitWindow.style.display = "block";
+    });
+};
+
+mySlider();
+onSubmit();
